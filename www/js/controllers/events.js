93c291ee41f72ddada967980aa59b8861d52fcd7
israel eduardo <israeleduardo@gmail.com>
2016-12-13 11:24:39 -0200

    ajustes eventsjs

--- www/js/controllers/events.js
+++ www/js/controllers/events.js
@@ -7,6 +7,7 @@ starter.controller('EventsCtrl', function($scope, $stateParams, $ionicPlatform, 
  $scope.SEARCH_TYPE_GPS = 1;
  $scope.SEARCH_TYPE_KEYBOARD = 2;
 
+
  $scope.baseUrlImage = 'http://www.snack4me.com/hotel/images/events/';
  $scope.baseUrlIcon  = 'img/ui/';
 
@@ -37,8 +38,65 @@ starter.controller('EventsCtrl', function($scope, $stateParams, $ionicPlatform, 
 
 
 
- $scope.getIntegration = function (id,event) {
+  $scope.vercpf = function(cpf) {
+      cpf = cpf.replace(/[^\d]+/g,'');
+      if(cpf == '') return false;
+      // Elimina CPFs invalidos conhecidos
+      if (cpf.length != 11 ||
+          cpf == "00000000000" ||
+          cpf == "11111111111" ||
+          cpf == "22222222222" ||
+          cpf == "33333333333" ||
+          cpf == "44444444444" ||
+          cpf == "55555555555" ||
+          cpf == "66666666666" ||
+          cpf == "77777777777" ||
+          cpf == "88888888888" ||
+          cpf == "99999999999")
+          return false;
+      // Valida 1o digito
+      add = 0;
+      for (i=0; i < 9; i ++)
+          add += parseInt(cpf.charAt(i)) * (10 - i);
+      rev = 11 - (add % 11);
+      if (rev == 10 || rev == 11)
+          rev = 0;
+      if (rev != parseInt(cpf.charAt(9)))
+          return false;
+      // Valida 2o digito
+      add = 0;
+      for (i = 0; i < 10; i ++)
+          add += parseInt(cpf.charAt(i)) * (11 - i);
+      rev = 11 - (add % 11);
+      if (rev == 10 || rev == 11)
+          rev = 0;
+      if (rev != parseInt(cpf.charAt(10)))
+          return false;
+      return true;
+  };
 
+    $scope.validarCPF = function(cpf) {
+
+        if ($scope.vercpf(cpf))
+        {
+            return true;
+        } else {
+            errors = "1";
+            if (errors){
+                $ionicPopup.alert({
+                    title: '',
+                    template: 'CPF Invádido!'
+                });
+                return false;
+            }
+
+        }
+    };
+
+
+
+ $scope.getIntegration = function (event) {
+     $scope.data = {};
      //ng-href="#/app/events/{{event.id}}/seats"
      $scope.selectEvent(event);
 
@@ -62,6 +120,7 @@ starter.controller('EventsCtrl', function($scope, $stateParams, $ionicPlatform, 
          });
 
          myPopup.then(function(res) {
+
              if(res) {
                  if(!$scope.data.cpf) {
                      $ionicPopup.alert({
@@ -69,13 +128,20 @@ starter.controller('EventsCtrl', function($scope, $stateParams, $ionicPlatform, 
                          template: 'CPF é obrigatório!'
                      });
                      return false;
+                 }else{
+                     if($scope.validarCPF($scope.data.cpf)){
+                         $location.path('/app/events/'+ event.id +'/seats?cpf=' + $scope.data.cpf);
+                     }
                  }
-                 $location.path('/app/events/'+ id +'/seats?cpf=' + $scope.data.cpf);
+
+
+
              }
          });
 
      }else{
-         $location.path('/app/events/'+ id +'/seats');
+         var c = '29659859805';//retorno da api de integracao
+         $location.path('/app/events/'+ event.id +'/seats?cpf=' + c);
      }
 
  }
